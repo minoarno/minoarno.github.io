358773a59b1d3125e0532de57abe841b40ec254d
Arno Poppe <minoarno@gmail.com>
2025-11-05 01:22:15 +0100

    tweaking the image

--- assets/OtherProjects-4046f373.js
+++ assets/OtherProjects-4046f373.js
@@ -1,4 +1,4 @@
-import{P as o,a as s}from"./ProjectData-0d32b9cd.js";import{d as a,_ as n,h as i,c,e as d,a as e,o as l}from"./index-cb16cca4.js";const p=[new o("project-7","DirectX 11 Engine","img/projects/zelda.png",`
+import{P as o,a as s}from"./ProjectData-219376e7.js";import{d as a,_ as n,h as i,c,e as d,a as e,o as l}from"./index-67bc5870.js";const p=[new o("project-7","DirectX 11 Engine","img/projects/zelda.png",`
     <div class="paragraph">
                 This project was made for the graphics programming 2 course at <strong>DAE</strong>.</br>
                 For the demonstration of the features of the engine, I made a little student game using assets of "Zelda Wind Waker" from Nintendo.</br>
